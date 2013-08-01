5143bf926b86a321561965dc68af5feee41c35e9
Eric Li <efilm0@gmail.com>
2013-08-01 10:47:36 +0800

    Add warning text in posting comment page

--- app/views/home/comments/create.blade.php
+++ app/views/home/comments/create.blade.php
@@ -5,81 +5,87 @@
 	<h1>{{ $title }}</h1>
 </div>
 
-<dl>
-	<dt>{{ Lang::get('app.course_title') }}</dt>
-	<dd>
-		{{{ $course->title_en }}}
-		@if ($course->title_zh)
-			<br />{{{ $course->title_zh }}}
-		@endif
-	</dd>
-	<dt>{{ Lang::get('app.course_category') }}</dt>
-	<dd>{{ $course->category }}</dd>
-	<dt>{{ Lang::get('app.course_department') }}</dt>
-	<dd>
-		<a href="{{ $course->department->url }}">
-			{{{ $course->department->title_en }}}<br />
-			{{{ $course->department->title_zh }}}
-		</a>
-	</dd>
-	<dt>{{ Lang::get('app.course_level') }}</dt>
-	<dd>{{{ $course->level }}}</dd>
-</dl>
-<hr />
+<div class="alert alert-block">
+	{{ trans('app.comment_warning') }}
+</div>
 
-{{ Form::open(['route' => 'comments.store', 'method' => 'POST', 'class' => 'form-horizontal']) }}
-	<div class="control-group {{ $errors->has('semester') ? 'error' : '' }}">
-		{{ Form::label('semester', Lang::get('app.comment_semester'), array('class' => 'control-label')) }}
-		<div class="controls">
-			{{ Form::select('semester', $semesters, Input::old('semester')) }}
-			{{ $errors->first('semester', '<span class="help-inline">:message</span>') }}
+<div class="row">
+	{{ Form::open(['route' => 'comments.store', 'method' => 'POST', 'class' => 'span9 form-horizontal']) }}
+		<div class="control-group {{ $errors->has('semester') ? 'error' : '' }}">
+			{{ Form::label('semester', Lang::get('app.comment_semester'), array('class' => 'control-label')) }}
+			<div class="controls">
+				{{ Form::select('semester', $semesters, Input::old('semester')) }}
+				{{ $errors->first('semester', '<span class="help-inline">:message</span>') }}
+			</div>
 		</div>
-	</div>
-
-	<div class="control-group {{ $errors->has('instructor') ? 'error' : '' }}">
-		{{ Form::label('instructor', Lang::get('app.comment_instructor'), array('class' => 'control-label')) }}
-		<div class="controls">
-			{{ Form::text('instructor', Input::old('instructor')) }}
-			{{ $errors->first('instructor', '<span class="help-inline">:message</span>') }}
+	
+		<div class="control-group {{ $errors->has('instructor') ? 'error' : '' }}">
+			{{ Form::label('instructor', Lang::get('app.comment_instructor'), array('class' => 'control-label')) }}
+			<div class="controls">
+				{{ Form::text('instructor', Input::old('instructor')) }}
+				{{ $errors->first('instructor', '<span class="help-inline">:message</span>') }}
+			</div>
 		</div>
-	</div>
-
-	<div class="control-group {{ $errors->has('grade') ? 'error' : '' }}">
-		{{ Form::label('grade', Lang::get('app.comment_grade'), array('class' => 'control-label')) }}
-		<div class="controls">
-			{{ Form::select('grade', $grades, Input::old('grade')) }}
-			{{ $errors->first('grade', '<span class="help-inline">:message</span>') }}
+	
+		<div class="control-group {{ $errors->has('grade') ? 'error' : '' }}">
+			{{ Form::label('grade', Lang::get('app.comment_grade'), array('class' => 'control-label')) }}
+			<div class="controls">
+				{{ Form::select('grade', $grades, Input::old('grade')) }}
+				{{ $errors->first('grade', '<span class="help-inline">:message</span>') }}
+			</div>
 		</div>
-	</div>
-
-	<div class="control-group {{ $errors->has('workload') ? 'error' : '' }}">
-		{{ Form::label('workload', Lang::get('app.comment_workload'), array('class' => 'control-label')) }}
-		<div class="controls">
-			{{ Form::select('workload', $workloads, Input::old('workload')) }}
-			{{ $errors->first('workload', '<span class="help-inline">:message</span>') }}
+	
+		<div class="control-group {{ $errors->has('workload') ? 'error' : '' }}">
+			{{ Form::label('workload', Lang::get('app.comment_workload'), array('class' => 'control-label')) }}
+			<div class="controls">
+				{{ Form::select('workload', $workloads, Input::old('workload')) }}
+				{{ $errors->first('workload', '<span class="help-inline">:message</span>') }}
+			</div>
 		</div>
-	</div>
-
-	<div class="control-group {{ $errors->has('body') ? 'error' : '' }}">
-		{{ Form::label('body', Lang::get('app.comment_body'), array('class' => 'control-label')) }}
-		<div class="controls">
-			{{ Form::textarea('body', Input::old('body'), array('class' => 'input-block-level', 'rows' => '10')) }}
-			{{ $errors->first('body', '<span class="help-inline">:message</span>') }}
+	
+		<div class="control-group {{ $errors->has('body') ? 'error' : '' }}">
+			{{ Form::label('body', Lang::get('app.comment_body'), array('class' => 'control-label')) }}
+			<div class="controls">
+				{{ Form::textarea('body', Input::old('body'), array('class' => 'input-block-level', 'rows' => '10')) }}
+				{{ $errors->first('body', '<span class="help-inline">:message</span>') }}
+			</div>
 		</div>
-	</div>
-
-	<!-- reCAPTCHA -->
-	<div class="control-group {{ $errors->has('recaptcha_response_field') ? 'error' : '' }}">
-		<div class="controls">
-			{{ Form::captcha(array('theme' => 'white')) }}
-			{{ $errors->first('recaptcha_response_field', '<span class="help-inline">:message</span>') }}
+	
+		<!-- reCAPTCHA -->
+		<div class="control-group {{ $errors->has('recaptcha_response_field') ? 'error' : '' }}">
+			{{ Form::label('recaptcha_response_field', Lang::get('app.comment_recaptcha'), array('class' => 'control-label')) }}
+			<div class="controls">
+				{{ Form::captcha(array('theme' => 'white')) }}
+				{{ $errors->first('recaptcha_response_field', '<span class="help-inline">:message</span>') }}
+			</div>
 		</div>
-	</div>
+	
+		<div class="form-actions">
+			{{ Form::hidden('course_id', $course->id) }}
+			<button type="submit" class="btn btn-primary"><i class="icon-ok"></i> {{ Lang::get('app.comment_submit') }}</button>
+		</div>
+	{{ Form::close() }}
 
-	<div class="form-actions">
-		{{ Form::hidden('course_id', $course->id) }}
-		<button type="submit" class="btn btn-primary"><i class="icon-ok"></i> {{ Lang::get('app.comment_submit') }}</button>
+	<div class="span3 hidden-phone">
+		<dl>
+			<dt>{{ Lang::get('app.course_title') }}</dt>
+			<dd>
+				{{{ $course->title_en }}}
+				@if ($course->title_zh)
+					<br />{{{ $course->title_zh }}}
+				@endif
+			</dd>
+			<dt>{{ Lang::get('app.course_category') }}</dt>
+			<dd>{{ $course->category }}</dd>
+			<dt>{{ Lang::get('app.course_department') }}</dt>
+			<dd>
+				{{{ $course->department->title_en }}}<br />
+				{{{ $course->department->title_zh }}}
+			</dd>
+			<dt>{{ Lang::get('app.course_level') }}</dt>
+			<dd>{{{ $course->level }}}</dd>
+		</dl>
 	</div>
-{{ Form::close() }}
+</div>
 
 @stop

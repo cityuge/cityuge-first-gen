4bd0ee4b8db0a1420c1adfd673e5ac3718c3679f
Eric Li <efilm0@gmail.com>
2013-08-04 00:49:26 +0800

    Add the migration and seeder for adding assessment task columns

---
+++ app/database/migrations/2013_08_03_161116_AddAssessmentFieldsInCoursesTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddAssessmentFieldsInCoursesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('courses', function($table)
+		{
+			$table->boolean('assess_exam');
+			$table->boolean('assess_quiz');
+			$table->boolean('assess_report');
+			$table->boolean('assess_project');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('courses', function($table)
+		{
+			$table->dropColumn('assess_exam', 'assess_quiz', 'assess_report', 'assess_project');
+		});
+	}
+
+}
--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder {
 		//$this->call('UsersTableSeeder');
 		//$this->call('DepartmentsTableSeeder');
 		//$this->call('CoursesTableSeeder');
-		$this->call('OfferingsTableSeeder');
+		//$this->call('OfferingsTableSeeder');
+		$this->call('CourseAssessmentSeeder');
 	}
 
 }

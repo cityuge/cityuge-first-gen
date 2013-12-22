0eae091be107118c2d74ef7b2d0b22adf8444ef7
Eric Li <efilm0@gmail.com>
2013-12-22 18:30:24 +0800

    Remove trailing slash in a case which current URL is home with locale code.

--- app/libraries/LocaleHelper.php
+++ app/libraries/LocaleHelper.php
@@ -9,19 +9,23 @@ class LocaleHelper {
 	public static function getCurrentPageURLInLocale($locale) {
 		// if the input locale is default locale, set it to null
 		$configAvailableLocaleURL = Config::get('cityuge.availableLocaleURL');
-		if ($locale == $configAvailableLocaleURL[0]) {
+		if ($locale === $configAvailableLocaleURL[0]) {
 			$locale = null;
 		}
 		// get all URL segments
 		$segments = Request::segments();
 
-		// we are in home page
+		// we are in home page and no locale attached
 		if (count($segments) == 0) {
 			return rtrim(URL::to('/') . '/' . $locale, '/');
 		}
-		// we are not in home page and the URL contain locale
+		// the URL contain locale
 		if (array_search($segments[0], Config::get('cityuge.availableLocaleURL'))) {
 			array_shift($segments);
+			// not to add trailing slash if it is in home page
+			if (count($segments) === 0) {
+				return URL::to('/') . '/' . ($locale == null ? '' : $locale);
+			}
 			return URL::to('/') . '/' . ($locale == null ? '' : $locale . '/') . implode('/', $segments);
 		}
 		// we are not in home page and the URL don't contain locale

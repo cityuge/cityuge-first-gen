6dd8e0e56453a32b814b1799ff6188d24858c772
Eric Li <efilm0@gmail.com>
2014-08-05 08:47:26 +0800

    Use enum for courses table.

--- app/database/migrations/2013_05_29_222946_CreateCoursesTable.php
+++ app/database/migrations/2013_05_29_222946_CreateCoursesTable.php
@@ -1,44 +1,44 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateCoursesTable extends Migration {
+class CreateCoursesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('courses', function(Blueprint $table)
-		{
-			$table->engine = 'InnoDB';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
 
-			$table->increments('id')->unsigned();
-			$table->string('code', 10)->unique();
-			$table->string('title_en', 200);
-			$table->string('title_zh', 200)->nullable();
-			$table->string('category', 6);
-			$table->string('level', 10);
-			$table->integer('department_id')->unsigned();
-			$table->string('grading_pattern', 6)->default('STD');
+            $table->increments('id')->unsigned();
+            $table->string('code', 10)->unique();
+            $table->string('title_en', 200);
+            $table->string('title_zh', 200)->nullable();
+            $table->enum('category', array('AREA1', 'AREA2', 'AREA3', 'E', 'UNIREQ'));
+            $table->string('level', 10);
+            $table->integer('department_id')->unsigned();
+            $table->enum('grading_pattern', array('PF', 'STD', 'STD-PF'))->default('STD');
 
-			$table->index('title_en');
-			$table->index('title_zh');
-			$table->index('category');
-		});
-	}
+            $table->index('title_en');
+            $table->index('title_zh');
+            $table->index('category');
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('courses');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('courses');
+    }
 
 }

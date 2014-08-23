ed456c1c036b2500d70873cf1f17f82526d186f3
Eric Li <efilm0@gmail.com>
2014-08-23 23:20:43 +0800

    Fix Simplified Chinese UI string.

--- app/lang/zh-cn/static.php
+++ app/lang/zh-cn/static.php
@@ -4,14 +4,14 @@ return array(
 
     // Home
     'home_jumboTitle' => 'Semester A GE Add/drop',
-    'home_jumboDesc' => '2013/14 Semester A 的 GE 指定 Add/drop 日为八月廿日及廿一日。',
+    'home_jumboDesc' => '2013/14 Semester A 的 GE 指定 Add/drop 日为八月二十日及二十一日。',
     'home_jumboMore' => '教务处 <small>(ARRO)</small> 网页 <i class="fa fa-external-link"></i>',
 
     'home_leadParagraph' => '专为城大学生而设的精进教育课程分享平台。',
     'home_featureSearchTitle' => '搜寻课程',
-    'home_featureSearchDesc' => '<p>只需输入课程编号，即可取得课程数据。使用进阶搜寻功能更可按开办学期、学系、分类及评核项目来拣选心水课程。</p>',
+    'home_featureSearchDesc' => '<p>只需输入课程编号，即可取得课程数据。使用进阶搜寻功能更可按开办学期、学系、分类及评核项目来拣选喜欢的课程。</p>',
     'home_featureBrowseTitle' => '阅读评论',
-    'home_featureBrowseDesc' => '<p>报读课程前，可以先阅读前人的评论以深入了解课堂形式、工作量、导师评分差异、成绩分布等数据，避免中伏。</p>',
+    'home_featureBrowseDesc' => '<p>报读课程前，可以先阅读前人的评论以深入了解课堂形式、工作量、导师评分差异、成绩分布等数据，避免踩陷阱。</p>',
     'home_featureWriteTitle' => '分享心得',
     'home_featureWriteDesc' => '<p>完成课程后，分享你的心得。让其他人能参考你的经验。你的留言亦非常重要，愈多的留言使网站的数据更为准确。</p>',
 
